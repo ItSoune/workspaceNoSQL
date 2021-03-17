dade3a7a8068f1730167031b678b41063cfbdf24
yassine <bouchdi.yassine1@gmail.com>
2021-03-17 17:12:52 +0100

    Premier Pas Programmation

---
+++ NoSQLProject/src/NoSQLParcoursArbreListener.java
@@ -0,0 +1,21 @@
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Queue;
+
+import org.stringtemplate.v4.compiler.Bytecode.Instruction;
+
+public class NoSQLParcoursArbreListener extends NoSQLBaseListener {
+
+    Queue<String> sqlFragements = new ArrayDeque<>();
+    //Map<String, Variable> variables = new HashMap<>();
+
+    public Queue<String> getInstructionsQueue() {
+        return sqlFragements;
+    }
+
+
+	public void enterListit (NoSQLParser.ListitContext ctx) {
+		System.out.println(ctx.iteration(0));
+	}
+}
